079487a94c29b078ab2df00cb95b7f86ad902cde
Michael Kwardakov <kwardakov@gmail.com>
2016-09-26 22:10:59 +0300

    Second try

--- task2.rb
+++ task2.rb
@@ -1,18 +1,38 @@
 #!/usr/bin/env ruby
-require 'github'
+require 'github_api'
+require 'io/console'
 
 type = ARGV[0]
 repo = ARGV[1]
 
 # check for parameters
-if (type != "issues" && type != "pulls") || repo.match(/[[:alnum:]]+\/[[:alnum:]]+/) == nil
+if type.nil? || repo.nil? || (type != "issues" && type != "pulls") || repo.match(/[[:alnum:]]+\/[[:alnum:]]+/) == nil
     puts "Usage: "
     puts "Example: ruby myprog.rb issues openshift/origin"
+    exit 1
 end
+repo_user, repo_name = repo.scan(/[[:alnum:]]+/)
 
-# https://github.com/piotrmurach/github
-# https://developer.github.com/v3/pulls/
-# GET /repos/:owner/:repo/pulls
-# https://developer.github.com/v3/issues/
-# GET /orgs/:org/issues
-github = Github.repos
+# github authorization
+print "Please enter github user: "
+# github_user     = STDIN.gets.strip
+github_user     = 'mkwardakov'
+print "And password: "
+# github_password = STDIN.noecho(&:gets).strip
+github_password = ''
+
+# now ask github
+github = Github.new basic_auth: "#{github_user}:#{github_password}", user: repo_user, repo: repo_name
+begin
+    case type
+    when 'issues'
+        reply = github.
+        puts "#{repo} issues: closed, #{reply.body['total_count']} open"
+    when 'pulls'
+        reply = github.search.issues q: "user=#{repo_user}+repo=#{repo_name}"
+        puts "#{repo} pull requests: #{reply.body['total_count']}"
+    end
+rescue Github::Error::GithubError => err
+    puts err.message
+    exit 1
+end
